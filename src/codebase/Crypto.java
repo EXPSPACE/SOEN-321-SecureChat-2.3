a8cc634e413167569bbe3618b29497dba19b79a9
Sebastian Shah <sebshah@hotmail.com>
2015-12-05 15:48:12 -0500

    Changed crypto methods to non static.

--- src/codebase/Crypto.java
+++ src/codebase/Crypto.java
@@ -28,21 +28,33 @@ import javax.crypto.spec.IvParameterSpec;
 
 public class Crypto {
 	
+	private String connection;
+	private RSAPublicKey certPublicKey;   // public key of connection
+	private RSAPrivateKey certPrivateKey; // self private key
+	//private DHKeyPair dhKeyPair
+	private SecretKey sharedSecretKey;    //message enc/dec key
+	
 	/** DIFFIE-HELLMAN **/
 	//asymmetric key cryptography methods for generating diffie-hellman public/private key pairs
 	
-	
-	
+//	public static KeyPair generateDHKeyPair() {
+//		
+//	}
+//	 
+//	public static SecretKey generateSharedSecretKey(Dhpublic otherskey) {
+//		sharedSecretKey = ...
+//	}
+//	
 	
 	
 	
 	/** SIGNATURES **/
 	//methods using SHA-256 with RSA to sign/verify the diffie-hellman public key
 	
-	public static byte[] getSignature(PrivateKey privateKey, byte[] data) {
+	public byte[] getSignature(byte[] data) {
 		try {
 			Signature sig = Signature.getInstance("SHA256withRSA"); //set algorithm
-			sig.initSign(privateKey);  //set private key to sign
+			sig.initSign(certPrivateKey);  //set private key to sign
 			sig.update(data); //set data to sign
 			return sig.sign(); //return signature	
 		} catch (NoSuchAlgorithmException e) {
@@ -55,10 +67,10 @@ public class Crypto {
 		return null;
 	}
 	
-	public static boolean verifySignature(PublicKey publicKey, byte[] signature, byte[] data) {
+	public boolean verifySignature(byte[] signature, byte[] data) {
 		try {
 			Signature sig = Signature.getInstance("SHA256withRSA"); //set algorithm
-			sig.initVerify(publicKey);  //set public key to verify
+			sig.initVerify(certPublicKey);  //set public key to verify
 			sig.update(data); //set data to verify
 			return sig.verify(signature); //verifies signature	
 		} catch (NoSuchAlgorithmException e) {
@@ -74,22 +86,21 @@ public class Crypto {
 	/** CERTIFICATES **/
 	//methods for accessing certificate private/public keys 
 
-	public static RSAPublicKey getCertificatePublicKey(String filepath) {
+	public void loadCertificatePublicKey(String filepath) {
 		try {
 			FileInputStream fis = new FileInputStream(filepath);
 			CertificateFactory cf = CertificateFactory.getInstance("X.509");
 			Certificate cert = cf.generateCertificate(fis);
-			return (RSAPublicKey) cert.getPublicKey();
+			certPublicKey = (RSAPublicKey) cert.getPublicKey();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (CertificateException e) {
 			e.printStackTrace();
 		}
-		return null;
 	}
 	
 	
-	public static RSAPrivateKey getCertificatePrivateKey(String filepath) {
+	public void loadCertificatePrivateKey(String filepath) {
 		
 		//TODO get encoded private key from filepath (is key encrypted?)
 		byte[] encodedKey = null;
@@ -97,25 +108,24 @@ public class Crypto {
 		try {
 			KeyFactory kf = KeyFactory.getInstance("RSA");
 			KeySpec keySpec = new PKCS8EncodedKeySpec(encodedKey);
-			return (RSAPrivateKey) kf.generatePrivate(keySpec);
+			certPrivateKey = (RSAPrivateKey) kf.generatePrivate(keySpec);
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		} catch (InvalidKeySpecException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return null;
 	}
 	
 	/** AES CIPHER **/
 	//symmetric key cryptography methods for encrypting/decrypting exchanged messages
 	
-	public static byte[] getEncryptedMessage(SecretKey secretKey, byte[] message) {
+	public byte[] getEncryptedMessage(byte[] message) {
 		Cipher cipher;
 		byte[] iv = new byte[16]; //TODO : Generate unique iv each time message is encrypted (prevents replay attacks?)
 		try {
 			cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
+			cipher.init(Cipher.ENCRYPT_MODE, sharedSecretKey, new IvParameterSpec(iv));
 			return cipher.doFinal(message);
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
@@ -134,11 +144,11 @@ public class Crypto {
 	}
 	
 	
-	public static byte[] getDecryptedMessage(SecretKey secretKey, byte[] message, byte[] iv) {
+	public byte[] getDecryptedMessage(byte[] message, byte[] iv) {
 		Cipher cipher;
 		try {
 			cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+			cipher.init(Cipher.DECRYPT_MODE, sharedSecretKey, new IvParameterSpec(iv));
 			return cipher.doFinal(message);
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
